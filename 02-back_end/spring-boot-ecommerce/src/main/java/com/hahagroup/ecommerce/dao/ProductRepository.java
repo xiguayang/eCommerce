6daef6fb736280ca568387bd86b204a1d1bffdd0
xiguayang <yangzhangamber1990@gmail.com>
2021-10-19 21:36:27 -0700

    frontend angular to access products and list them
    1. Create Angular Project.
    2. Create Angular component for product-list
    3. Develop TypeScript class for Product(modify tsconfig.json)
    4. Create Angular service to call REST APIs
    5. Update Angular component ProducListComponent to subscribe to data from Angular Service
    6. Update HTML page to show product list
    7. back end Spring Boot needs to add CrossOrigin with frontend

--- 02-back_end/spring-boot-ecommerce/src/main/java/com/hahagroup/ecommerce/dao/ProductRepository.java
+++ 02-back_end/spring-boot-ecommerce/src/main/java/com/hahagroup/ecommerce/dao/ProductRepository.java
@@ -3,8 +3,10 @@ package com.hahagroup.ecommerce.dao;
 import com.hahagroup.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 //entity type is Product, and primary key type is Long
 //@RepositoryRestResource(collectionResourceRel = "product", path="products")
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product,Long> {
 }
