49ad9b57334a1fb22f0322dcc286ecd492499792
xiguayang <yangzhangamber1990@gmail.com>
2021-11-02 14:06:30 -0700

    modify folder name

--- 03-frontend/anguler-ecommerce/src/app/config/my-app-config.ts
+++
@@ -1,8 +0,0 @@
-export default{
-    oidc:{
-        clientId:'0oa2h9u28zBk7d3An5d7',
-        issuer:'https://dev-40532106.okta.com/oaut2/default',
-        redirectUri:'http://localhost:4200/login/callback',
-        scopes:['openid','profile','email']
-    }
-} 
